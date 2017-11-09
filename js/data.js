7b0f60f5cac708a4e6320158cab795830cf7d1e6
ricking84 <ricking84@yahoo.com>
2017-11-09 09:42:31 -0600

    First Commit

---
+++ js/data.js
@@ -0,0 +1,14 @@
+    var data = '{"June": [{"Type": "COM", "valuation": "$39,830", "Address": "124+SW+8TH+ST,+Bentonville,+AR",' +
+        '"Description": "COMMERCIAL ALTERATION/REMODEL","Latitude": "36.36424300000001","Longitude": "-94.20907039999997"},' +
+        '{"Type": "COM","valuation": "$341500","Address": "3001 SE J ST Bentonville, AR 72712","Description": "COMMERCIAL ALTERATION/REMODEL",' +
+        '"Latitude": "36.3397049","Longitude": "-94.19682879999999"},' +
+        '{"Type": "COM","valuation": "$148,000","Address": "3301 S WALTON BLVD STE 1 Bentonville, AR 72712","Description": "COMMERCIAL ALTERATION/REMODEL",' +
+        '"Latitude": "36.3387781","Longitude": "-94.2101927"},' +
+        '{"Type": "COM","valuation": "$50,594","Address": "805 MCCLAIN RD STE 8002 Bentonville, AR 72712",' +
+        '"Description": "COMMERCIAL ALTERATION/REMODEL","Latitude": "36.3791","Longitude": "-94.17071899999996"},' +
+        '{"Type": "COM","valuation": "$142,204","Address": "901 MCCLAIN RD STE 7 Bentonville, AR 72712",' +
+        '"Description": "COMMERCIAL ALTERATION/REMODEL","Latitude": "36.3795718","Longitude": "-94.17169960000001"},' +
+        '{"Type": "COM","valuation": "$839,840","Address": "1327 S MAIN ST Bentonville, AR 72712",' +
+        '"Description": "NEW STORES AND CUSTOMER SERVICES","Latitude": "36.35765960000001","Longitude": "-94.2091709"},' +
+        '{"Type": "COM","valuation": "$375,840","Address": "1335 S MAIN ST Bentonville, AR 72712",' +
+        '"Description": "NEW STORES AND CUSTOMER SERVICES","Latitude": "36.3575824","Longitude": "-94.20917380000003"}]}';
---
+++ js/map.js
@@ -0,0 +1,37 @@
+function initMap() {
+    var obj = JSON.parse(data);
+    var June = obj.June
+    var map;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: new google.maps.LatLng(36.3575824, -94.20917380000003),
+        mapTypeId: 'terrain'
+    });
+
+    for (i in June) {
+        var Address = June[i].Address
+        console.log(Address)
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + Address + "&key=AIzaSyCvD82CsHfL--vFtotMwu5oo7ttRrZMXWE";
+        fetch(url)
+            .then(res => res.json())
+            .then((out) => {
+                console.log('Checkout this JSON! ', out);
+            })
+            .catch(err => { throw err });
+        var lati = June[i].Latitude;
+        var lng = June[i].Longitude;
+        //console.log(lati + "," + lng)
+       // console.log(geo)
+
+        var latiLng = new google.maps.LatLng(lati, lng);
+        var marker = new google.maps.Marker({
+            position: latiLng,
+            map: map
+        });
+    }
+}
+
+$("#residential").hover(function () {
+    $(this).fadeTo('fast', 0.5);
+});
