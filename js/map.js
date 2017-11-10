d834d5d382afeb79677b8ee4eda918695a9eba80
ricking84 <ricking84@yahoo.com>
2017-11-09 19:43:34 -0600

    Fixed part of the auth issue

--- js/map.js
+++ js/map.js
@@ -1,30 +1,26 @@
 function initMap() {
     var obj = JSON.parse(data);
     var June = obj.June
-    var map;
-    var googleMapsAuth = "poop"
-    console.log(googleMapsAuth)
-    map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: new google.maps.LatLng(36.3575824, -94.20917380000003),
         mapTypeId: 'terrain'
     });
 
     for (i in June) {
-        
+
         var Address = June[i].Address
-        console.log(Address)
-        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + Address + "&key=AIzaSyCvD82CsHfL--vFtotMwu5oo7ttRrZMXWE";
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + Address + "&key=" + googleMapsAuth;
         console.log(url)
-         fetch(url)
+        fetch(url)
             .then(res => res.json())
-             .then((out) => {
-                console.log('Checkout this JSON! ', out);
-        })
-        .catch(err => { throw err });
+            .then((out) => {
+                console.log(out);
+            })
+            .catch(err => { throw err });
         var lati = June[i].Latitude;
         var lng = June[i].Longitude;
-        console.log(lati + "," + lng)
+       // console.log(lati + "," + lng)
 
         var latiLng = new google.maps.LatLng(lati, lng);
         var marker = new google.maps.Marker({
@@ -32,8 +28,4 @@ function initMap() {
             map: map
         });
     }
-}
-
-$("#residential").hover(function () {
-    $(this).fadeTo('fast', 0.5);
-});
+};
