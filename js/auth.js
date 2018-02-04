cfb085583c605b3a3aed529c9e4150a79ffc9b93
ricking84 <ricking84@yahoo.com>
2018-02-04 15:55:04 -0600

    noclue

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,7 @@
 var googleMapsAuth = "AIzaSyCvD82CsHfL--vFtotMwu5oo7ttRrZMXWE"
+
+var Serv = "kingrealty.database.windows.net"
+
+var userName = "<rking84@kingrealty>"
+
+var PWord = "<London1984>"
---
+++ js/realty.sql
@@ -0,0 +1,16 @@
+SELECT *
+FROM [RealtyData]
+
+-- INSERT INTO [RealtyData]
+-- ([Id],[IssueDate],[Year],[Month],[Type]
+-- ,[Description],[Address],[Valuation])
+-- VALUES
+-- ('','','',''
+-- ,'','','','');
+
+-- UPDATE [RealtyData]
+-- SET [RealtyData].[Id] = ' '
+-- WHERE Type = 'CAL'
+
+-- DELETE FROM [RealtyData].[Id]
+-- WHERE Type = 'CAL'
